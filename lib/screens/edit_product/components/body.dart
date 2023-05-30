b46a45b24ca1363b43d9112de41a6d44e0075033
siying <siying060202@gmail.com>
2023-05-30 21:53:56 +0800

    ver1.1

--- lib/screens/edit_product/components/body.dart
+++ lib/screens/edit_product/components/body.dart
@@ -23,7 +23,7 @@ class Body extends StatelessWidget {
               children: [
                 SizedBox(height: getProportionateScreenHeight(10)),
                 Text(
-                  "Fill Product Details",
+                  "Fill Packages Details ",
                   style: headingStyle,
                 ),
                 SizedBox(height: getProportionateScreenHeight(30)),
--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -30,29 +30,29 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   final productCategories = <Map>[
     <String, dynamic>{
-      ICON_KEY: "assets/icons/Electronics.svg",
-      TITLE_KEY: "Electronics",
-      PRODUCT_TYPE_KEY: ProductType.Electronics,
+      ICON_KEY: "assets/icons/Helicopter.svg",
+      TITLE_KEY: "Helicopter",
+      PRODUCT_TYPE_KEY: ProductType.Helicopter,
     },
     <String, dynamic>{
-      ICON_KEY: "assets/icons/Books.svg",
-      TITLE_KEY: "Books",
-      PRODUCT_TYPE_KEY: ProductType.Books,
+      ICON_KEY: "assets/icons/CruiseShip.svg",
+      TITLE_KEY: "Cruise Ship",
+      PRODUCT_TYPE_KEY: ProductType.CruiseShip,
     },
     <String, dynamic>{
-      ICON_KEY: "assets/icons/Fashion.svg",
-      TITLE_KEY: "Fashion",
-      PRODUCT_TYPE_KEY: ProductType.Fashion,
+      ICON_KEY: "assets/icons/HotAirBalloon.svg",
+      TITLE_KEY: "Hot Air Balloon",
+      PRODUCT_TYPE_KEY: ProductType.HotAirBalloon,
     },
     <String, dynamic>{
-      ICON_KEY: "assets/icons/Groceries.svg",
-      TITLE_KEY: "Groceries",
-      PRODUCT_TYPE_KEY: ProductType.Groceries,
+      ICON_KEY: "assets/icons/Paragliding.svg",
+      TITLE_KEY: "Paragliding",
+      PRODUCT_TYPE_KEY: ProductType.Paragliding,
     },
     <String, dynamic>{
-      ICON_KEY: "assets/icons/Art.svg",
-      TITLE_KEY: "Art",
-      PRODUCT_TYPE_KEY: ProductType.Art,
+      ICON_KEY: "assets/icons/WhitewaterRafting.svg",
+      TITLE_KEY: "Whitewater Rafting",
+      PRODUCT_TYPE_KEY: ProductType.WhitewaterRafting,
     },
     <String, dynamic>{
       ICON_KEY: "assets/icons/Others.svg",
@@ -197,9 +197,9 @@ class _BodyState extends State<Body> {
                 SizedBox(
                   height: SizeConfig.screenHeight * 0.5,
                   child: ProductsSection(
-                    sectionTitle: "Products You Like",
+                    sectionTitle: "Packages You Like",
                     productsStreamController: favouriteProductsStream,
-                    emptyListMessage: "Add Product to Favourites",
+                    emptyListMessage: "Add Packages to Favourites",
                     onProductCardTapped: onProductCardTapped,
                   ),
                 ),
@@ -207,7 +207,7 @@ class _BodyState extends State<Body> {
                 SizedBox(
                   height: SizeConfig.screenHeight * 0.8,
                   child: ProductsSection(
-                    sectionTitle: "Explore All Products",
+                    sectionTitle: "Explore All Packages",
                     productsStreamController: allProductsStream,
                     emptyListMessage: "Looks like all Stores are closed",
                     onProductCardTapped: onProductCardTapped,
